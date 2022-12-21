1339638c93041d2525fba6774809550daeca4645
emilyalvae <emilisa23@gmail.com>
2022-12-20 23:40:22 -0500

    config db

--- storage/settings.py
+++ storage/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'storage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': '********',
+        'HOST': 'containers-us-west-128.railway.app',
+        'PORT': '7823',
     }
 }
 
